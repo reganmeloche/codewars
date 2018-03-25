5b6a051d4cb09b0bec18e2d12b655e7a3afc2d52
reganmeloche <rmeloche@gasbuddy.com>
2018-03-25 13:19:39 -0600

    Create fold_to_moon.java

---
+++ level7/fold_to_moon.java
@@ -0,0 +1,42 @@
+/*
+
+Have you heard about the myth that if you fold a paper enough times, you can reach the moon with it? Sure you do, but exactly how many? Maybe it's time to write a program to figure it out.
+
+You know that a piece of paper has a thickness of 0.0001m. Given distance in units of meters, calculate how many times you have to fold the paper to make the paper reach this distance.
+(If you're not familiar with the concept of folding a paper: Each fold doubles its total thickness.)
+
+Note: Of course you can't do half a fold. You should know what this means ;P
+
+Also, if somebody is giving you a non-positive distance, it's clearly bogus and you should yell at them by returning null (or whatever equivalent in your language).
+
+*/
+
+public class PaperFolder {
+  public static Long fold(Double distance) {
+    if ( distance < 0) {
+      return null;
+    }
+    
+    Double curr = 0.0001;
+    Long folds = 0L;
+    
+    while (curr < distance) {
+      curr = curr * 2;
+      folds++;
+    }
+    return folds;
+  }
+}
+
+// Test
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+  public void sampleTests() {
+    assertEquals(Long.valueOf(42L), PaperFolder.fold(new Double(384000000)));
+    assertEquals(Long.valueOf(0L), PaperFolder.fold(new Double(0.00005)));
+  }
+}
